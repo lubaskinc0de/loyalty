564f4bb801635d2ff56a85d1454a7fa8b9f82adf
lubaskin <lubaskincorporation@gmail.com>
2025-03-20 22:57:15 +0300

    gunicorn

--- src/loyalty/bootstrap/entrypoint/flask_api.py
+++ src/loyalty/bootstrap/entrypoint/flask_api.py
@@ -1,18 +1,29 @@
+import multiprocessing
 import sys
 
+import gunicorn.app.wsgiapp  # type: ignore
 from dishka.integrations.flask import setup_dishka
 from flask import Flask
 
 from loyalty.bootstrap.di.container import get_container
 from loyalty.presentation.web.flask_api.root import include_root
 
+flask_app = Flask(__name__)
+
 
 def main(_args: list[str]) -> None:
-    flask_app = Flask(__name__)
     include_root(flask_app)
 
     setup_dishka(container=get_container(), app=flask_app, auto_inject=True)
-    flask_app.run(port=5000, host="0.0.0.0")
+    sys.argv = [  # ugly hack
+        "gunicorn",
+        "-b",
+        "0.0.0.0:5000",
+        "-w",
+        str(multiprocessing.cpu_count() * 2),
+        "loyalty.bootstrap.entrypoint.flask_api:flask_app",
+    ]
+    gunicorn.app.wsgiapp.run()
 
 
 if __name__ == "__main__":
