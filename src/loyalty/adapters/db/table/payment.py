370d5c0588aaf3d2bf5a3cf6781b125b9e9e7896
lubaskin <lubaskincorporation@gmail.com>
2025-05-10 23:30:05 +0300

    payment model

---
+++ src/loyalty/adapters/db/table/payment.py
@@ -0,0 +1,45 @@
+import sqlalchemy as sa
+
+from loyalty.adapters.db.registry import mapper_registry
+from loyalty.domain.entity.payment import Payment
+
+metadata = mapper_registry.metadata
+
+payment_table = sa.Table(
+    "payment",
+    metadata,
+    sa.Column("payment_id", sa.UUID(as_uuid=True), primary_key=True),
+    sa.Column(
+        "client_id",
+        sa.UUID(as_uuid=True),
+        sa.ForeignKey("client.client_id", ondelete="SET NULL"),
+        nullable=True,
+    ),
+    sa.Column(
+        "loyalty_id",
+        sa.UUID(as_uuid=True),
+        sa.ForeignKey("loyalty.loyalty_id", ondelete="SET NULL"),
+        nullable=True,
+    ),
+    sa.Column(
+        "membership_id",
+        sa.UUID(as_uuid=True),
+        sa.ForeignKey("loyalty_membership.membership_id", ondelete="SET NULL"),
+        nullable=True,
+    ),
+    sa.Column(
+        "business_id",
+        sa.UUID(as_uuid=True),
+        sa.ForeignKey("business.business_id", ondelete="SET NULL"),
+        nullable=True,
+    ),
+    sa.Column("payment_sum", sa.Numeric(10, 2), nullable=False),
+    sa.Column("service_income", sa.Numeric(10, 2), nullable=False),
+    sa.Column("bonus_income", sa.Integer, nullable=False),
+    sa.Column("created_at", sa.DateTime(timezone=True)),
+)
+
+mapper_registry.map_imperatively(
+    Payment,
+    payment_table,
+)
