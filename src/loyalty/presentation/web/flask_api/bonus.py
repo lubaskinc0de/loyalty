769fa0f72f3601200b1b23676b921f81e6fdb017
lubaskin <lubaskincorporation@gmail.com>
2025-05-13 19:04:40 +0300

    calc discount route

--- src/loyalty/presentation/web/flask_api/bonus.py
+++ src/loyalty/presentation/web/flask_api/bonus.py
@@ -1,8 +1,10 @@
+from decimal import Decimal
 from uuid import UUID
 
 from dishka import FromDishka
-from flask import Blueprint, Response, jsonify
+from flask import Blueprint, Response, jsonify, request
 
+from loyalty.application.bonus.discount import CalcDiscount, CalcDiscountData
 from loyalty.application.bonus.read import ReadBonuses
 from loyalty.presentation.web.serializer import serializer
 
@@ -13,3 +15,17 @@ bonus = Blueprint("bonus", __name__)
 def read_bonuses(membership_id: UUID, interactor: FromDishka[ReadBonuses]) -> Response:
     result = interactor.execute(membership_id)
     return jsonify(serializer.dump(result))
+
+
+@bonus.route("/discount", methods=["GET"], strict_slashes=False)
+def calc_discount(interactor: FromDishka[CalcDiscount]) -> Response:
+    membership_id = request.args.get("membership_id", type=UUID)
+    purchase_amount = request.args.get("purchase_amount", type=Decimal)
+
+    calc_discount_data = CalcDiscountData.model_validate(
+        {
+            "membership_id": membership_id,
+            "purchase_amount": purchase_amount,
+        },
+    )
+    return jsonify(serializer.dump(interactor.execute(calc_discount_data)))
