5a87535d341d10a2de862cb8ef1dbd0bdec0624e
lubaskin <lubaskincorporation@gmail.com>
2025-05-15 02:42:57 +0300

    business stats better tests and docs model

--- tests/integration/business/test_stats.py
+++ tests/integration/business/test_stats.py
@@ -1,4 +1,5 @@
 import asyncio
+import random
 from collections.abc import Coroutine
 from decimal import ROUND_DOWN, Decimal
 from typing import Any
@@ -7,11 +8,12 @@ from uuid import uuid4
 import pytest
 
 from loyalty.adapters.api_client import APIResponse, LoyaltyClient
-from loyalty.adapters.api_models import LoyaltyId
+from loyalty.adapters.api_models import LoyaltyId, MembershipId
+from loyalty.application.data_model.business_branch import BusinessBranchData
 from loyalty.application.loyalty.create import LoyaltyForm
-from loyalty.application.membership.dto import MembershipData
-from loyalty.application.payment.create import PaymentCreated
-from tests.conftest import BusinessUser
+from loyalty.application.membership.create import MembershipForm
+from loyalty.application.payment.create import PaymentCreated, PaymentForm
+from tests.conftest import BusinessUser, ClientUser
 
 
 @pytest.mark.parametrize(
@@ -27,21 +29,21 @@ from tests.conftest import BusinessUser
 async def test_ok(
     api_client: LoyaltyClient,
     business: BusinessUser,
-    membership: MembershipData,  # noqa: ARG001
-    another_membership: MembershipData,  # noqa: ARG001
-    payment: PaymentCreated,
-    another_payment: PaymentCreated,
+    client: ClientUser,
     loyalties_count: int,
     loyalty_form: LoyaltyForm,
+    branch: BusinessBranchData,
 ) -> None:
-    api_client.authorize(business[2])
+    client_obj, _, client_token = client
 
-    payments_amount = payment.payment_sum + another_payment.payment_sum
-    waste_amount = payment.service_income + another_payment.service_income
-    bonus_given = payment.bonus_income + another_payment.bonus_income
+    payments_amount = Decimal(0)
+    waste_amount = Decimal(0)
+    bonus_given = Decimal(0)
 
     loyalty_names = [uuid4().hex[:99] for _ in range(loyalties_count)]
-    tasks: list[Coroutine[Any, Any, APIResponse[LoyaltyId]]] = []
+    tasks_loyalty: list[Coroutine[Any, Any, APIResponse[LoyaltyId]]] = []
+    tasks_membership: list[Coroutine[Any, Any, APIResponse[MembershipId]]] = []
+    tasks_payment: list[Coroutine[Any, Any, APIResponse[PaymentCreated]]] = []
 
     for name in loyalty_names:
         form = loyalty_form.model_copy(
@@ -49,16 +51,44 @@ async def test_ok(
                 "name": name,
             },
         )
-        tasks.append(api_client.create_loyalty(form))
+        tasks_loyalty.append(api_client.create_loyalty(form))
 
-    for each in await asyncio.gather(*tasks):
-        each.except_status(200).unwrap()
+    api_client.authorize(business[2])
+    for loyalty_req in await asyncio.gather(*tasks_loyalty):
+        loyalty_id = loyalty_req.except_status(200).unwrap()
+        tasks_membership.append(
+            api_client.create_membership(
+                MembershipForm(
+                    loyalty_id=loyalty_id.loyalty_id,
+                ),
+            ),
+        )
+    api_client.authorize(client_token)
+    for memberhip_req in await asyncio.gather(*tasks_membership):
+        membership_id = memberhip_req.except_status(200).unwrap().membership_id
+        payment_sum = Decimal(random.randint(100, 1000))  # noqa: S311
+        tasks_payment.append(
+            api_client.create_payment(
+                PaymentForm(
+                    payment_sum=payment_sum,
+                    membership_id=membership_id,
+                    business_branch_id=branch.business_branch_id,
+                    client_id=client_obj.client_id,
+                ),
+            ),
+        )
+    api_client.authorize(business[2])
+    for payment_req in await asyncio.gather(*tasks_payment):
+        payment_info = payment_req.except_status(200).unwrap()
+        payments_amount += payment_info.payment_sum
+        waste_amount += payment_info.service_income
+        bonus_given += payment_info.bonus_income
 
     resp = (await api_client.read_business_stats()).except_status(200).unwrap()
 
     assert resp.bonus_given_amount == bonus_given.quantize(Decimal("0.01"), rounding=ROUND_DOWN)
-    assert resp.loyalties_count == loyalties_count + 1
-    assert resp.memberships_count == 2
+    assert resp.loyalties_count == loyalties_count
+    assert resp.memberships_count == len(tasks_membership)
     assert resp.payments_amount == payments_amount.quantize(Decimal("0.01"), rounding=ROUND_DOWN)
     assert resp.waste_amount == waste_amount.quantize(Decimal("0.01"), rounding=ROUND_DOWN)
 
