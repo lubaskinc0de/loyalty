8e4c49e1b2a11054ae5247ab816a56bf471836f2
lubaskin <lubaskincorporation@gmail.com>
2025-05-15 20:34:12 +0300

    image remove on detach

--- tests/integration/business/test_detach.py
+++ tests/integration/business/test_detach.py
@@ -1,18 +1,29 @@
 from pathlib import Path
 
+from aiohttp import ClientSession
+
 from loyalty.adapters.api_client import LoyaltyClient
-from tests.conftest import BusinessUser, ClientUser
+from tests.conftest import BusinessUser, ClientUser, relative_url
 
 
-async def test_ok(api_client: LoyaltyClient, business: BusinessUser, image_file: Path) -> None:
+async def test_ok(
+    api_client: LoyaltyClient,
+    business: BusinessUser,
+    image_file: Path,
+    http_session: ClientSession,
+) -> None:
     src_business, _, token = business
     api_client.authorize(token)
 
     (await api_client.attach_business_avatar(image_file)).except_status(200)
-    assert (await api_client.read_business(src_business.business_id)).unwrap().avatar_url is not None
+    avatar_url = (await api_client.read_business(src_business.business_id)).unwrap().avatar_url
+    assert avatar_url is not None
 
     (await api_client.detach_business_avatar()).except_status(204)
     assert (await api_client.read_business(src_business.business_id)).unwrap().avatar_url is None
+
+    async with http_session.get(relative_url(avatar_url)) as r:
+        assert r.status == 404
 
 
 async def test_detach_empty(api_client: LoyaltyClient, business: BusinessUser) -> None:
